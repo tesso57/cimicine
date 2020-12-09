edf7e56bf5987c5871e89087669e4e941224bff9
UCQ <zerda.elsword@gmail.com>
2020-12-09 22:44:34 +0900

    fix:HeaderはAppから読み込むように

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,12 @@
-import React from "react"
+import React from "react";
 import Header from "./Header";
 
 const Home = () => {
-    return (
-        <>
-            <Header/>
-            <h1>HOME </h1>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>HOME </h1>
+    </>
+  );
+};
 
 export default Home;
