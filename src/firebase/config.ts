e54999b8b5973e2e308a126a1cf1ee225a7fc1d6
tesso <ta4780@icloud.com>
2020-12-09 21:21:18 +0900

    GitHub Actionsの設定

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBxdx_-6MbYZ82lMAZIL6UcLchUNbVxSK0",
+    authDomain: "cimicine-flow.firebaseapp.com",
+    projectId: "cimicine-flow",
+    storageBucket: "cimicine-flow.appspot.com",
+    messagingSenderId: "739722826994",
+    appId: "1:739722826994:web:258ee2289bd9c0d102ec6b",
+    measurementId: "G-ZM8Z0GVRK8"
+};
+
+export default firebaseConfig
