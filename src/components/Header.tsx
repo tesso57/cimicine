e970efe72f057bbcf467cd35693df28de9a47871
harsh <mizuki.k.aa@m.titech.ac.jp>
2020-12-10 01:59:15 +0900

    ログイン後のdrawerを作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,51 @@ import "./Header.css";
 import logo from "../img/logo.svg";
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import NoteAddIcon from "@material-ui/icons/NoteAdd";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import SearchIcon from "@material-ui/icons/Search";
 import { IconButton, Drawer } from "@material-ui/core";
 
 import { useHistory } from "react-router";
 const Menu: React.FC = () => {
-  return <div className="header__menu"></div>;
+  return (
+    <div className="header__menu">
+      <svg viewBox="0 0 500 500" className="logo">
+        <use xlinkHref={`${logo}#logo`} />
+      </svg>
+
+      <div className="createButton">
+        <div className="buttonContent">
+          <NoteAddIcon fontSize="large" className="createMapsIcon" />
+          <span>作成</span>
+        </div>
+      </div>
+
+      <hr className="partition" />
+
+      <div className="hotMaps">
+        <div className="hotMapsContent">
+          <WhatshotIcon
+            style={{ color: "#ff1f1f" }}
+            fontSize="large"
+            className="hotMapsIcon"
+          />
+          <span>急上昇のロードマップ</span>
+        </div>
+      </div>
+
+      <div className="searchMaps">
+        <div className="searchMapsContent">
+          <SearchIcon
+            style={{ color: "#007c40" }}
+            fontSize="large"
+            className="searchMapsIcon"
+          />
+          <span>ロードマップを探す</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const Header: React.FC = () => {
