e1655c8e03d7ec83860b5c7c1566df84f86cbfbd
UCQ <zerda.elsword@gmail.com>
2020-12-09 22:38:39 +0900

    add:Headerを追加＆その他ごちゃごちゃしたやつ

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "./App.css";
-import {AuthProvider} from "./auth/AuthProvider";
-import { BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import { AuthProvider } from "./auth/AuthProvider";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./auth/Login";
 import PrivateRoute from "./auth/PrivateRoute";
 import Home from "./components/Home";
 import SignUp from "./auth/SignUp";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
-      <AuthProvider>
-          <Router>
-              <Switch>
-                  <PrivateRoute exact path ={"/"} component={Home}/>
-                  <Route exact path={"/login"} component={Login}/>
-                  <Route exact path={"/signup"} component={SignUp}/>
-              </Switch>
-          </Router>
-      </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <PrivateRoute exact path={"/"} component={Home} />
+          <Route exact path={"/login"} component={Login} />
+          <Route exact path={"/signup"} component={SignUp} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,2 +1,35 @@
 import React from "react";
 import "./Header.css";
+
+import logo from "../img/logo.svg";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { IconButton } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+const Header: React.FC = () => {
+  const history = useHistory();
+  return (
+    <div className="header">
+      <IconButton>
+        <MenuIcon fontSize="large" />
+      </IconButton>
+
+      <div className="logoAndTitle" onClick={() => history.push("/")}>
+        <svg viewBox="0 0 500 500" className="logo">
+          <use xlinkHref={`${logo}#logo`} />
+        </svg>
+        <h1 className="title">Cimicine</h1>
+      </div>
+      <IconButton>
+        <AccountCircleIcon
+          className="userIcon"
+          fontSize="large"
+          style={{ color: "#007c40" }}
+        />
+      </IconButton>
+    </div>
+  );
+};
+
+export default Header;
