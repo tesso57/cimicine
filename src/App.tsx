0007eeb4e0a001dd49410f8e0565f0a620794c01
tesso <ta4780@icloud.com>
2020-12-10 00:49:25 +0900

    外観作成完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import "./App.css";
-import { AuthProvider } from "./auth/AuthProvider";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {AuthProvider} from "./auth/AuthProvider";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import SignIn from "./auth/SignIn";
 import PrivateRoute from "./auth/PrivateRoute";
 import Home from "./components/Home";
 import SignUp from "./auth/SignUp";
 import Header from "./components/Header";
+import MakeNewLoadMap from "./components/MakeNewLoadMap";
 
 const App: React.FC = () => {
-  return (
-    <AuthProvider>
-      <Header />
-      <Router>
-        <Switch>
-          <PrivateRoute exact path={"/"} component={Home} />
-          <Route exact path={"/signin"} component={SignIn} />
-          <Route exact path={"/signup"} component={SignUp} />
-        </Switch>
-      </Router>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <Header/>
+                <Switch>
+                    <PrivateRoute exact path={"/"} component={MakeNewLoadMap}/>
+                    <Route exact path={"/signin"} component={SignIn}/>
+                    <Route exact path={"/signup"} component={SignUp}/>
+                </Switch>
+            </Router>
+        </AuthProvider>
+    );
 };
 
 export default App;
---
+++ src/components/MakeNewLoadMap.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import KeyboardBackspaceSharpIcon from '@material-ui/icons/KeyboardBackspaceSharp';
+import "./MakeNewLoadMap.css"
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+import {
+    createMuiTheme,
+    ThemeProvider
+} from '@material-ui/core/styles';
+import {green} from '@material-ui/core/colors';
+
+const MakeNewLoadMap: React.FC = () => {
+    const theme = createMuiTheme({
+        palette: {
+            primary: green,
+        },
+    });
+    return (
+
+        <div className={"makeNewLoadMapContent"}>
+            <KeyboardBackspaceSharpIcon fontSize="large"/>
+
+            <div className={"heading"}>新しいロードマップを作成する</div>
+            <div className={"input-textField"}>
+                <TextField
+                    id="outlined-password-input"
+                    label="タイトル"
+                    type="title"
+                    autoComplete="current-password"
+                    variant="outlined"
+                    className={"title-inputField"}
+                />
+            </div>
+
+            <div className={"input-textField"}>
+                <TextField
+                    id="outlined-password-input"
+                    label="詳細"
+                    type="title"
+                    autoComplete="current-password"
+                    variant="outlined"
+                    fullWidth
+                />
+            </div>
+
+            <div>
+                <ThemeProvider theme={theme}>
+                    <Button variant="contained" color="primary" style={{color:'white' , marginTop:48}}>
+                        作成
+                    </Button>
+                </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default MakeNewLoadMap
