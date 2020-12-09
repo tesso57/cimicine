18466fcbd6d542832629c981c63bf121a5a9738e
harsh <mizuki.k.aa@m.titech.ac.jp>
2020-12-09 20:30:37 +0900

    headerを作成

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,2 +1,23 @@
 import React from "react";
 import "./Header.css";
+
+import logo from "../img/logo.svg"
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+function Header() {
+  return (
+    <div className="header">
+      <MenuIcon className="menuIcon" style={{ width: '24px', height: 'auto' }}/>
+      <div className="logoAndTitle">
+        <svg viewBox="0 0 500 500" className="logo">
+            <use xlinkHref={`${logo}#logo`} />
+          </svg>
+        <h1 className="title">Cimicine</h1>
+      </div>
+      <AccountCircleIcon style={{ color: '#007c40', width: '38px', height: 'auto' }} className="userIcon" />
+    </div>
+  )
+}
+
+export default Header;
