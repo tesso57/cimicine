21986b12b24ac6d6c930f192d7ad3bede34bdc09
UCQ <zerda.elsword@gmail.com>
2020-12-10 02:35:57 +0900

    add:ロードマップのtemplate

--- src/components/EditRoadmap.tsx
+++ src/components/EditRoadmap.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./EditRoadmap.css";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import AssistantPhotoIcon from "@material-ui/icons/AssistantPhoto";
 import PauseIcon from "@material-ui/icons/Pause";
 import { IconButton } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import zIndex from "@material-ui/core/styles/zIndex";
 
 interface EditRoadmapProps {
   title?: string;
@@ -15,6 +17,7 @@ const EditRoadmap: React.FC<EditRoadmapProps> = ({
   setTitle,
 }) => {
   const history = useHistory();
+  const sampleText = "The First Step";
   return (
     <div>
       <div className="nav">
@@ -24,12 +27,49 @@ const EditRoadmap: React.FC<EditRoadmapProps> = ({
         <h1>{title}</h1>
       </div>
       <div className="edit">
-        <div className="edgePoint">
+        <div className="edgePoint begin">
           <p>{title}</p>
           <PlayArrowIcon style={{ color: "var(--cimicine-main)" }} />
         </div>
         <div className="border"></div>
-        <div className="edgePoint">
+
+        <div className="step">
+          <AssistantPhotoIcon
+            fontSize="large"
+            style={{
+              color: "var(--cimicine-main",
+              backgroundColor: "white",
+              padding: "4 0",
+              zIndex: 5,
+            }}
+          />
+          <p>{sampleText}</p>
+        </div>
+        <div className="step">
+          <AssistantPhotoIcon
+            fontSize="large"
+            style={{
+              color: "var(--cimicine-main",
+              backgroundColor: "white",
+              padding: "4 0",
+              zIndex: 5,
+            }}
+          />
+          <p>{sampleText}</p>
+        </div>
+        <div className="step">
+          <AssistantPhotoIcon
+            fontSize="large"
+            style={{
+              color: "var(--cimicine-main",
+              backgroundColor: "white",
+              padding: "4 0",
+              zIndex: 5,
+            }}
+          />
+          <p>{sampleText}</p>
+        </div>
+        <div className="edgePoint finish">
           <p>{title}</p>
           <PauseIcon style={{ color: "var(--cimicine-main)" }} />
         </div>
