bf5eb31fac53e31a9ef1226d828c0ef26e99ed25
UCQ <zerda.elsword@gmail.com>
2020-12-12 21:19:43 +0900

    fix:急上昇-->急上昇中に変更

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,37 +2,36 @@ import SearchIcon from "@material-ui/icons/Search";
 import React from "react";
 import { Card } from "./Card";
 import "./Home.css";
-import {trendMockData} from "../utils/mock";
+import { trendMockData } from "../utils/mock";
 import TextField from "@material-ui/core/TextField";
 
 const Home: React.FC = () => {
-    return (
-        <div className="home">
-            <div className={"search"}>
-                <SearchIcon fontSize="large"/>
-                <TextField
-                    InputProps={{
-                        'aria-label': 'naked',
-                        disableUnderline: true,
-                        style: {
-                            color: `#000`,
-                            fontSize: 20,
-                            fontWeight:"bold"
-                        }
-                    }}
-                    placeholder={"ロードマップを探す"}
-                    fullWidth={true}
-                />
-            </div>
-            <h1 style={{color: "white", margin: "24px 0 16px"}}>
-                🔥 急上昇のロードマップ
-            </h1>
-            {trendMockData.map((d, index) => (
-                <Card title={d.title} star={d.star} caption={d.caption} key={index}/>
-            ))}
-
-        </div>
-    );
+  return (
+    <div className="home">
+      <div className={"search"}>
+        <SearchIcon fontSize="large" />
+        <TextField
+          InputProps={{
+            "aria-label": "naked",
+            disableUnderline: true,
+            style: {
+              color: `#000`,
+              fontSize: 20,
+              fontWeight: "bold",
+            },
+          }}
+          placeholder={"ロードマップを探す"}
+          fullWidth={true}
+        />
+      </div>
+      <h1 style={{ color: "white", margin: "24px 0 16px" }}>
+        🔥 急上昇中のロードマップ
+      </h1>
+      {trendMockData.map((d, index) => (
+        <Card title={d.title} star={d.star} caption={d.caption} key={index} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
