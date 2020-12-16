ab5904fcf0eb85a6ed19e19fb7fc0558f682255c
UCQ <zerda.elsword@gmail.com>
2020-12-16 11:09:10 +0900

    fix:footerの位置を修正

--- src/App.tsx
+++ src/App.tsx
@@ -20,17 +20,23 @@ const App: React.FC = () => {
       <AuthProvider>
         <Router>
           <Header />
-          <Switch>
-            <Route exact path={"/signin"} component={SignIn} />
-            <Route exact path={"/signup"} component={SignUp} />
-            <PrivateRoute exact path={"/edit"} component={EditRoadmap} />
-            <PrivateRoute exact path={"/create"} component={MakeNewLoadMap} />
-            <PrivateRoute exact path={"/view"} component={ViewRoadmap} />
-            <PrivateRoute path={"/view/:uid"} component={ViewRoadmap} />
-            <PrivateRoute path={"/profile/:displayName"} component={Profile} />
-            <PrivateRoute exact path={"/"} component={Home} />
-            <Route component={NotFound} />
-          </Switch>
+          <div className="switch">
+            <Switch>
+              <Route exact path={"/signin"} component={SignIn} />
+              <Route exact path={"/signup"} component={SignUp} />
+              <PrivateRoute exact path={"/edit"} component={EditRoadmap} />
+              <PrivateRoute exact path={"/create"} component={MakeNewLoadMap} />
+              <PrivateRoute exact path={"/view"} component={ViewRoadmap} />
+              <PrivateRoute path={"/view/:uid"} component={ViewRoadmap} />
+              <PrivateRoute
+                path={"/profile/:displayName"}
+                component={Profile}
+              />
+              <PrivateRoute exact path={"/"} component={Home} />
+              <Route component={NotFound} />
+            </Switch>
+            <div className="footer__push"></div>
+          </div>
           <Footer />
         </Router>
       </AuthProvider>
