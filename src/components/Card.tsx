e7d037cd9c14bede5da70c9da528dabb3ccfdb23
UCQ <zerda.elsword@gmail.com>
2020-12-16 13:17:47 +0900

    fix:オーバーレイ表示できるように

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,17 +2,23 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { JsonTypes } from "../type";
 import "./Card.css";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ShareIcon from "@material-ui/icons/Share";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { Button, IconButton, Tooltip } from "@material-ui/core";
 
 interface CardProps {
   data: JsonTypes;
 }
 export const Card: React.FC<CardProps> = ({ data }) => {
   const history = useHistory();
+  const copyUrl = async () => {
+    await navigator.clipboard.writeText(
+      `https://cimicice-flow.web.app/view/${data.data.uid}`
+    );
+  };
   return (
-    <div
-      className="card"
-      onClick={() => history.push("/view/" + data.data.uid)}
-    >
+    <div className="card">
       <h2
         className="title"
         style={{ color: "black", opacity: 0.87, fontSize: 18 }}
@@ -21,6 +27,26 @@ export const Card: React.FC<CardProps> = ({ data }) => {
       </h2>
       <p className="star">★ Star {data.data.star}</p>
       <p className="description">{data.data.description}</p>
+      <div className="card__cover">
+        <Tooltip title="Copy URL">
+          <IconButton style={{ color: "white" }} onClick={copyUrl}>
+            <FileCopyIcon style={{ color: "white" }} fontSize="small" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Share">
+          <IconButton style={{ color: "white" }}>
+            <ShareIcon style={{ color: "white" }} fontSize="small" />
+          </IconButton>
+        </Tooltip>
+        <div className="card__divider"></div>
+        <Button
+          style={{ border: "1px solid white", color: "white" }}
+          onClick={() => history.push(`/view/${data.data.uid}`)}
+        >
+          show
+          <KeyboardArrowRightIcon />
+        </Button>
+      </div>
     </div>
   );
 };
